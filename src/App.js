044d6d15f57b7b99b83ca453d9e90c3f8ff5e86d
ignaciocarre <ignacio.carre@gmail.com>
2020-04-06 13:57:28 -0300

    Fix consulta hernan
    - Se creo un archivo nuevo para ilustrar el cambio de color de fondo
    - Se alinearon los elementos con Grid
    
    Issue: no

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
 import SignInSide from "./components/SignInSide";
+import {makeStyles} from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import NewSignIn from "./components/NewSignIn";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import {MuiThemeProvider} from "@material-ui/core";
+
+const myTheme = createMuiTheme({
+    overrides: {
+        MuiCssBaseline: {
+            '@global': {
+                body: {
+                    backgroundColor: '#88f2a7',
+                },
+            },
+        },
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-        <SignInSide/>
-    </div>
-  );
+    return (
+        <MuiThemeProvider theme={myTheme}>
+            <CssBaseline/>
+            <NewSignIn/>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
